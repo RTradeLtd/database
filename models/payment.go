0dbf6c01c1dbb387ee1ed203ec63541fb10965d1
potsables <alextrottier2014@gmail.com>
2018-10-07 07:55:10 -0700

    models: bring payment in sync with temporal

--- models/payment.go
+++ models/payment.go
@@ -9,14 +9,14 @@ import (
 // Payments is our payment model
 type Payments struct {
 	gorm.Model
+	Number         int64   `gorm:"type:integer"`
 	DepositAddress string  `gorm:"type:varchar(255)"`
 	TxHash         string  `gorm:"type:varchar(255)"`
-	USDValue       float64 `gorm:"type:varchar(255)"` // USDValue is also a "Credit" value, since 1 USD -> 1 Credit
+	USDValue       float64 `gorm:"type:float"` // USDValue is also a "Credit" value, since 1 USD -> 1 Credit
 	Blockchain     string  `gorm:"type:varchar(255)"`
 	Type           string  `gorm:"type:varchar(255)"` // ETH, RTC, XMR, BTC, LTC
 	UserName       string  `gorm:"type:varchar(255)"`
 	Confirmed      bool    `gorm:"type:varchar(255)"`
-	Processing     bool    `gorm:"type:varchar(255)"`
 }
 
 // PaymentManager is used to interact with payment information in our database
@@ -29,25 +29,39 @@ func NewPaymentManager(db *gorm.DB) *PaymentManager {
 	return &PaymentManager{DB: db}
 }
 
-// FindPaymentByTxHash is used to find a payment by its TxHash
-func (pm *PaymentManager) FindPaymentByTxHash(txHash string) (*Payments, error) {
+// GetLatestPaymentNumber is used to get the latest payment number for a user
+func (pm *PaymentManager) GetLatestPaymentNumber(username string) (int64, error) {
 	p := Payments{}
-	if check := pm.DB.Where("tx_hash = ?", txHash).First(&p); check.Error != nil {
-		return nil, check.Error
+	check := pm.DB.Where("user_name = ?", username).Order("number desc").First(&p)
+	if check.Error != nil && check.Error != gorm.ErrRecordNotFound {
+		return 0, check.Error
 	}
-	return &p, nil
+
+	if check.Error == gorm.ErrRecordNotFound {
+		return 0, nil
+	}
+	return p.Number + 1, nil
 }
 
 // NewPayment is used to create a payment in our database
-func (pm *PaymentManager) NewPayment(depositAddress string, txHash string, usdValue float64, blockchain string, paymentType string, username string) (*Payments, error) {
+func (pm *PaymentManager) NewPayment(number int64, depositAddress string, txHash string, usdValue float64, blockchain string, paymentType string, username string) (*Payments, error) {
 	p := Payments{}
-	check := pm.DB.Where("tx_hash = ?", txHash).First(&p)
-	if check.Error == nil {
-		return nil, errors.New("payment with tx hash already exists")
-	} else if check.Error != nil && check.Error != gorm.ErrRecordNotFound {
+	// check for a payment with the number
+	check := pm.DB.Where("number = ?", number).First(&p)
+	if check.Error != nil && check.Error != gorm.ErrRecordNotFound {
 		return nil, check.Error
 	}
-
+	if check.Error == nil {
+		return nil, errors.New("payment with number already exists in database")
+	}
+	// check for a payment with the tx hash
+	check = pm.DB.Where("tx_hash = ?", txHash).First(&p)
+	if check.Error != nil && check.Error != gorm.ErrRecordNotFound {
+		return nil, check.Error
+	}
+	if check.Error == nil {
+		return nil, errors.New("paymnet with tx hash already exists in database")
+	}
 	p = Payments{
 		DepositAddress: depositAddress,
 		TxHash:         txHash,
@@ -56,7 +70,7 @@ func (pm *PaymentManager) NewPayment(depositAddress string, txHash string, usdVa
 		Type:           paymentType,
 		UserName:       username,
 		Confirmed:      false,
-		Processing:     false,
+		Number:         number,
 	}
 
 	if check := pm.DB.Create(&p); check.Error != nil {
@@ -77,20 +91,4 @@ func (pm *PaymentManager) ConfirmPayment(txHash string) (*Payments, error) {
 		return nil, check.Error
 	}
 	return &p, nil
-}
-
-// MarkPaymentAsProcessing is sued to mark a payment as being processed
-func (pm *PaymentManager) MarkPaymentAsProcessing(txHash string) (*Payments, error) {
-	payment, err := pm.FindPaymentByTxHash(txHash)
-	if err != nil {
-		return nil, err
-	}
-	if payment.Processing {
-		return nil, errors.New("payment is already being processed")
-	}
-	payment.Processing = true
-	if check := pm.DB.Model(payment).Update("processing", payment.Processing); check.Error != nil {
-		return nil, check.Error
-	}
-	return payment, nil
 }
