a9d6ce982907a5a4a1c83e5bf7599ef968f11a95
Robert Lin <robertlin1@gmail.com>
2019-02-04 13:27:23 -0800

    Update models/ipns.go
    Co-Authored-By: postables <postables@rtradetechnologies.com>

--- models/ipns.go
+++ models/ipns.go
@@ -47,7 +47,7 @@ func (im *IpnsManager) FindByUserName(username string) (*[]IPNS, error) {
 
 // FindAll is used to find all IPNS records
 func (im *IpnsManager) FindAll() ([]IPNS, error) {
-	entries := &[]IPNS{}
+	entries := []IPNS{}
 	if err := im.DB.Model(&IPNS{}).Find(entries).Error; err != nil {
 		return nil, err
 	}
